3a47694e3d9fb17fe16785ee0c7869a97535decc
Mika <mika.terhokoski@gmail.com>
2020-06-30 17:28:13 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,152 @@
+import shlex
+import mqtt_wrap
+from prompt_toolkit import PromptSession
+
+mqtt = None
+exit_program = False
+
+def cmd_exit(params):
+    global exit_program
+    exit_program = True
+
+def cmd_help(params):
+    for i, v in commands.items():
+        print(f"{i:16s} {v[1]}")
+
+def cmd_echo(params):
+    print(str(params))
+
+def cmd_mqtt_sub(params):
+    if len(params) != 1:
+        print("Usage:\nsub <topic>")
+        return
+
+    print(f"Subscribing to topic: {params[0]} ")
+
+def cmd_mqtt_unsub(params):
+    if len(params) != 1:
+        print("Usage:\nunsub <topic>\nunsub all")
+        return
+
+    if params[0] == "all":
+        print(f"Unsubscribing from all topics")
+    else:
+        print(f"Unsubscribing from topic: {params[0]} ")
+
+def cmd_mqtt_pub(params):
+    if len(params) != 2:
+        print("Usage:\npub <topic> [msg]")
+        return
+
+    print(f"Publishing '{params[1]}' to '{params[0]}'")
+    mqtt.send_msgs([params[0]], params[1])
+
+def cmd_mqtt_ping(params):
+    if len(params) != 1:
+        print("Usage:\nping <uid>\nping all")
+        return
+
+    def ping(uid):
+        try:
+            t = mqtt.gen_topic(uid=uid, name="sys", topic="ping")
+            r = mqtt.gen_topic(uid=uid, name="sys", topic="pong")
+            mqtt.send_and_wait(t, "", r)
+            print(f"{uid} OK")
+        except TimeoutError:
+            print(f"{uid} Timeout")
+        except KeyError:
+            print(f"{uid} Unknown device")
+
+    if params[0] == "all":
+        for uid in mqtt_wrap.devices:
+            ping(uid)
+    else:
+        ping(params[0])
+
+def cmd_write_file(params):
+    if len(params) != 3:
+        print("Usage:\nwrite <uid> <local-file> <remote-file>")
+        return
+
+    def write_file(uid, local, remote):
+        try:
+            with open(local, "r") as f:
+                data = f.read()
+
+            f = mqtt.gen_topic(uid=uid, name="sys", topic="file")
+            w = mqtt.gen_topic(uid=uid, name="sys", topic="write")
+            r = mqtt.gen_topic(uid=uid, name="sys", topic="resp")
+            mqtt.send_msg(f, remote)
+            mqtt.send_and_wait(w, data, r)
+
+            print(f"{uid} OK")
+        except TimeoutError:
+            print(f"{uid} Timeout")
+        except KeyError:
+            print(f"{uid} Unknown device")
+
+    uid, local, remote = params
+    write_file(uid, local, remote)
+
+
+def cmd_read_file(params):
+    if len(params) != 3:
+        print("Usage:\nread <uid> <remote-file> <local-file>")
+        return
+
+    def read_file(uid, local, remote):
+        try:
+            f = mqtt.gen_topic(uid=uid, name="sys", topic="file")
+            w = mqtt.gen_topic(uid=uid, name="sys", topic="read")
+            r = mqtt.gen_topic(uid=uid, name="sys", topic="resp")
+            mqtt.send_msg(f, remote)
+            data = mqtt.send_and_wait(w, "", r)
+
+            with open(local, "w") as f:
+                f.write(data)
+
+            print(f"{uid} OK")
+        except TimeoutError:
+            print(f"{uid} Timeout")
+        except KeyError:
+            print(f"{uid} Unknown device")
+
+    uid, remote, local = params
+    read_file(uid, local, remote)
+  
+
+commands = {
+    "help": (cmd_help, "Prints this help"),
+    "exit": (cmd_exit, "Exits the program"),
+    "echo": (cmd_echo, "Echos back paramters for testing"),
+    #"sub":  (cmd_mqtt_sub, "Subscribe to MQTT topic"),
+    #"unsub":  (cmd_mqtt_unsub, "Unsubscribe from MQTT topic"),
+    "pub":  (cmd_mqtt_pub, "Publish to MQTT topic"),
+    "ping": (cmd_mqtt_ping, "Test connection to device"),
+    "write": (cmd_write_file, "Write file to a device"),
+    "read" : (cmd_read_file, "Read file from device"),
+}
+
+def run_commad(cmd):
+    if cmd == "":
+        return 
+
+    params = shlex.split(cmd)
+    try:
+        commands[params[0]][0](params[1:])
+    except KeyError:
+        print(f"Command not found: {cmd}")
+
+
+if __name__ == "__main__":
+    mqtt = mqtt_wrap.mqtt("localhost")
+    #mqtt = mqtt_wrap.mqtt("10.0.0.4")
+
+    p = PromptSession()
+
+    while not exit_program:
+        cmd = p.prompt("> ")
+        run_commad(cmd)
+
+    print("Bye")
+
---
+++ mqtt_wrap.py
@@ -0,0 +1,97 @@
+import sys
+import time
+import paho.mqtt.client
+import logging
+
+log = logging.getLogger("MQTT")
+
+class device:
+    def __init__(self, func, name):
+        self.name = name
+        self.func = func
+
+devices = {
+    "00ff00ff" : device("train", "netlin juna"),
+    "abbaabba" : device("hopper", "kaljaaaaa"),
+    "deadbeef" : device("hopper", "jallua"),
+    "94b91400" : device("switch", "Testi vaihde"),
+}
+
+class mqtt:
+    def __init__(self, ip="localhost"):
+        log.info("creating new instance")
+        self.mqtt = paho.mqtt.client.Client() #create new instance
+        log.info("connecting to broker")
+        self.mqtt.connect(ip) #connect to broker
+        log.info("Connected")
+        self.mqtt.loop_start()
+
+    def gen_topic_list(self, uid=None, name=None, topic=None):
+        t = []
+        topic = "/" + topic if topic else ""
+
+        #If uid is empty send to all devices
+        if uid == None:
+            for key, value in devices.items():
+                t.append(f"{key}/{value.func}{topic}")
+
+        #If name is empty populate it automatically 
+        elif name == None:
+            t = [f"{uid}/{devices[uid].func}{topic}"]
+        else:
+            t = [f"{uid}/{name}{topic}"]
+
+        return t
+
+    def gen_topic(self, uid, name=None, topic=None):
+        topic = "/" + topic if topic else ""
+
+        #If name is empty populate it automatically 
+        if name == None:
+            return f"{uid}/{devices[uid].func}{topic}"
+        else:
+            return f"{uid}/{name}{topic}"
+
+    def send_msg(self, topic, msg):
+        log.debug(topic, msg)
+        self.mqtt.publish(topic, msg)
+
+    def send_msgs(self, topic_list, msg):
+        for topic in topic_list:
+            self.send_msg(topic, msg)
+
+    def _wait_msg(self, topic, timeout=1):
+        reply = ""
+        done = False
+        def __wait_callback(client, userdata, message):
+            nonlocal reply, done
+            reply = message.payload
+            done = True
+
+        self.mqtt.subscribe(topic)
+        self.mqtt.message_callback_add(topic, __wait_callback)
+
+        while not done:
+            time.sleep(0.02)
+            timeout -= 0.02
+            if timeout < 0:
+                raise TimeoutError()
+
+        self.mqtt.unsubscribe(topic)
+        return reply.decode()
+
+    def sub(self, uid, name, topic, callback):
+        t = f"{uid}/{name}/{topic}"
+        self.mqtt.subscribe(t)
+        self.mqtt.message_callback_add(t, callback)
+
+    def send_and_wait_list(self, topic_list, msg, resp_topic_list, timeout=1):
+        response = []
+        for a in zip(topic_list, resp_topic_list):
+            self.send_msg(a[0], msg)
+            response.append(self._wait_msg(a[1], timeout))
+        return response
+
+    def send_and_wait(self, topic, msg, resp_topic, timeout=1):
+        self.send_msg(topic, msg)
+        return self._wait_msg(resp_topic, timeout)
